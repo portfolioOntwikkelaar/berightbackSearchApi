91162e0820e5227386c9acfb41e3213e0f63cb02
noordseoostende <voropayev1@mail.ru>
2022-02-12 16:26:51 +0100

    We hebben de acteurs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css'
+import Header from './components/ui/Header';
+import Search from './components/ui/Search';
 
 const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async ( ) => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+
+  }, [])
   return (
     <div className="container">
-      <h1>FireStorm programm</h1>
+      <Header />
+      <Search />
     </div>
   );
 }
