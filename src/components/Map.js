7d0a46e917954129cd87dbce1bc53d4247bfebdd
noordseoostende <voropayev1@mail.ru>
2022-02-12 15:58:32 +0100

    be right back)

---
+++ src/components/Map.js
@@ -0,0 +1,14 @@
+import GoogleMapReact from 'google-map-react'
+
+function Map({center, zoom}) {
+  return (
+    <div className="map">
+      <GoogleMapReact>
+         
+      </GoogleMapReact>
+      
+    </div>
+  )
+}
+
+export default Map
